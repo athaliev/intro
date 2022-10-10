0124a49b7a37993c4767f80d1bf4e5ceb8520a3f
Athalie Ventura <athalie@MacBook-Pro-de-Athalie.local>
2022-10-10 16:33:11 -0500

    intro declaracion,inicializacion, asignacion, comparacion, ciclo

---
+++ hello_node/hello.js
@@ -0,0 +1,23 @@
+console.log("Hello World!")
+
+// declaracion
+var name = "athalie"
+// declaracion + inicializacion
+var count = 0
+// asignacion
+count = 3
+// count = count + 4
+count += 4
+count *= 3
+// interpolacion
+console.log(`count ${count}`)
+
+// comparacion i<3
+// i++ ==> i = i + 1
+for(var i=0; i<3; i++){
+    console.log(`step ${i+1}`)
+}
+
+for(var i=0; i<6; i+=2){
+    console.log(`step ${i+1}`)
+}

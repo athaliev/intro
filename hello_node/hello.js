df5d0e6933c725551e833d3a34351e17943cf7a9
Athalie Ventura <athalie@MacBook-Pro-de-Athalie.local>
2022-10-12 15:23:57 -0500

    funciones, branching, switch

--- hello_node/hello.js
+++ hello_node/hello.js
@@ -7,6 +7,7 @@ var count = 0
 // asignacion
 count = 3
 // count = count + 4
+// operadores ++, +=, --, -=, *=, /=, %=
 count += 4
 count *= 3
 // interpolacion
@@ -18,6 +19,42 @@ for(var i=0; i<3; i++){
     console.log(`step ${i+1}`)
 }
 
-for(var i=0; i<6; i+=2){
-    console.log(`step ${i+1}`)
+// i+=2 ==> i = i + 2
+for(var i=0; i<8; i+=2){
+    // i<8 comparison
+    //if, else if, else ==> branching
+   if (i < 4) console.log(`step ${i} < 4`)
+   else if (i < 6) console.log(`step ${i} < 6`)
+   else console.log(`step ${i} >= 6`)
+}
+
+for(var i=0; i<8; i++){
+    if (i%2==0) console.log(`even ${i}`)
+    else console.log(`odd ${i}`)
+}
+
+// four ways to write a function
+// functions are also call lamdas
+function nonzeroprime(i) {
+    console.log("non zero prime: " + i)
+}
+var nonzeroprime2 = function() {
+    console.log("non zero prime")
+}
+var nonzeroprime3 = () => {
+    console.log("non zero prime")
+}
+var nonzeroprime4 = () => console.log("non zero prime")
+
+for(var i=0; i<5; i++){
+   switch(i) {
+        case 0: {
+            console.log("zero") 
+            break
+        }
+        //case 1: console.log("one"); break
+        case 2: nonzeroprime(i); break
+        case 3: nonzeroprime(i); break
+        default: console.log("other"); break
+   }
 }
